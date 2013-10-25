1c449e246b315995206a11fef1abbc53afdf3231
Angel Kurten <angelkurten@hotmail.com>
2013-10-25 06:22:30 -0500

    e

--- inc/cone.php
+++ inc/cone.php
@@ -2,4 +2,11 @@
 	$serverName = "xak2ftvng9.database.windows.net,1433"; //serverName\instanceName, portNumber (por defecto es 1433)
 	$connectionInfo = array( "Database"=>"girubd", "UID"=>"girubd", "PWD"=>"Angelkurten93");
 	$conn = sqlsrv_connect( $serverName, $connectionInfo);
+	if( $conn ) {
+?>
+	<script>console.log('Conexión establecida.')</script>
+<?php
+	}else{
+	     die( print_r( sqlsrv_errors(), true));
+	}
 ?>
