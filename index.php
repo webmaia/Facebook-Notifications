b58010f932b6e41fbc3590ad64d3658f8b3b96d3
Angel Kurten <angelkurten@hotmail.com>
2013-10-25 02:20:02 -0500

    diseño

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
+<!DOCTYPE HTML>
 <html>
 <head>
   <meta charset="UTF-8" />
+  <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600" rel="stylesheet" type="text/css" />
+  <!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/jquery.dropotron.js"></script>
+    <script src="js/skel.min.js"></script>
+    <script src="js/skel-panels.min.js"></script>
+    <script src="js/init.js"></script>
+    <noscript>
+      <link rel="stylesheet" href="css/skel-noscript.css" />
+      <link rel="stylesheet" href="css/style.css" />
+      <link rel="stylesheet" href="css/style-desktop.css" />
+      <link rel="stylesheet" href="css/style-noscript.css" />
+    </noscript>
+    <!--[if lte IE 8]><link rel="stylesheet" href="css/ie8.css" /><![endif]-->
 </head>
-<body>
+<body class="homepage">
     <div id="fb-root"></div>
     <script>
       window.fbAsyncInit = function() {
