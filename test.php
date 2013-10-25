6a8511743b509e075f9ac95ac32e33eee4ceac3b
Angel Kurten <angelkurten@hotmail.com>
2013-10-25 05:55:01 -0500

    test

--- test.php
+++ test.php
@@ -1,37 +1,12 @@
 <?php
-      include('src/facebook.php');
-     
 
-      $config = array();
-      $config['appId'] = '514448035289505';
-      $config['secret'] = '0077cc0f2364adea04a39e5d9dbd4337';
-      $config['grant_type']='client_credentials';
+   	$serverName = "xak2ftvng9.database.windows.net,1433"; //serverName\instanceName, portNumber (por defecto es 1433)
+	$connectionInfo = array( "Database"=>"girubd", "UID"=>"girubd", "PWD"=>"Angelkurten93");
+	$conn = sqlsrv_connect( $serverName, $connectionInfo);
 
-      $facebook = new Facebook($config);  
-
-      $user = $facebook->getUser();
-
-      if ($user) {
-        try {
-          // Proceed knowing you have a logged in user who's authenticated.
-          $user = $facebook->api('/me');
-        } catch (FacebookApiException $e) {
-          error_log($e);
-          $user = null;
-        }
-      }
-
-      $facebook->setAccessToken($config['appId'].'|'.$config['secret']);
-
-      $userID = $user['id'];
-      $message = 'Prueba aplicacion para enviar notificaciones';
-      $href = '?live=yes';
-      'ref' => 'Notification sent '.$dNow->format("Y-m-d G:i:s") //this is for Facebook's insight
-
-      $params = array(
-              'href' => $href,
-              'template' => $message,
-          );
-
-      //$facebook->api('/' . $userID . '/notifications/', 'post', $params);
-    ?>
+	if( $conn ) {
+	     echo "Conexión establecida.<br />";
+	}else{
+	     echo "Conexión no se pudo establecer.<br />";
+	     die( print_r( sqlsrv_errors(), true));
+	}
