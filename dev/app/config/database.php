cb5e845f2ed9444987643b29a41241309693ac28
Angel Kurten <angelkurten@hotmail.com>
2013-10-29 15:48:10 -0500

    dev

--- dev/app/config/database.php
+++ dev/app/config/database.php
@@ -49,10 +49,10 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
-$db['default']['dbdriver'] = 'mysql';
+$db['default']['username'] = 'girubd';
+$db['default']['password'] = 'Angelkurten93';
+$db['default']['database'] = 'girubd';
+$db['default']['dbdriver'] = 'sqlsrv';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
