6938e0dbb107f720d2a01c0566770adafa597ff8
Angel Kurten <angelkurten@hotmail.com>
2013-10-29 13:52:26 -0500

    dev
    redireccion

--- dev/app/views/redireccion.php
+++ dev/app/views/redireccion.php
@@ -1,1 +1,1 @@
-<script>top.location.href = 'https://www.facebook.com/dialog/oauth?client_id=514448035289505&redirect_uri=http://apps.facebook.com/girucode/&scope=publish_stream'</script>
+<script>top.location.href = 'https://www.facebook.com/dialog/oauth?client_id=212654485574232&redirect_uri=http://apps.facebook.com/girucodebeta/&scope=publish_stream'</script>
