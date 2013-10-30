879e023b102e0dd202b6eaea4b1a8198cc4ea194
Angel Kurten <angelkurten@hotmail.com>
2013-10-30 02:06:41 -0500

    dev

--- dev/app/helpers/facebook_helper.php
+++ dev/app/helpers/facebook_helper.php
@@ -8,7 +8,7 @@ if ( ! function_exists('enviar_notificacion'))
 		//var_dump($CI->facebook);
 		if (is_array($destino))
 		{
-	        $facebook->setAccessToken($CI->config->appId.'|'.$CI->config->secret);
+	        $CI->facebook->setAccessToken($CI->config->appId.'|'.$CI->config->secret);
 	        $params = array(
 	           'href' => $href,
                 'template' => $mensaje,
