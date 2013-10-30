5ca06b91af43e1df281c0fab4ef9801d86e73634
Angel Kurten <angelkurten@hotmail.com>
2013-10-30 02:23:07 -0500

    dev

--- dev/app/config/facebook.php
+++ dev/app/config/facebook.php
@@ -2,9 +2,10 @@
 	$config['appId']  = '212654485574232';
 	$config['secret'] = '43f516956439f1a03fb60dddd5eb0afe';
 	$config['grant_type']='client_credentials';
+
 	$facebook = new Facebook($config);
 	
-	public function tokenFacebook()
+	function tokenFacebook()
 	{
 		return $facebook->setAccessToken($config['appId'].'|'.$config['secret']);
 	}
