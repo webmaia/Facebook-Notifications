e22c85d5e2703d58fc46f1efa228ee6351b4faeb
Angel Kurten <angelkurten@hotmail.com>
2013-10-29 23:06:44 -0500

    helper dev

---
+++ dev/app/helpers/facebook_helper.php
@@ -0,0 +1,19 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+if ( ! function_exists('enviar_notificacion'))
+{
+	function enviar_notificacion($mensaje, $destino,$href='?live=yes')
+	{
+		if (is_array($destino))
+		{
+	        $params = array(
+	           'href' => $href,
+                'template' => $mensaje,
+            );
+			
+			while($destino) {
+              $facebook->api('/' . $destino['id'] . '/notifications/', 'post', $params);
+          	}
+		}
+	}
+}
