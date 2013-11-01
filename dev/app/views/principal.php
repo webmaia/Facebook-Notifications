6fc4ad83828220855f9a8660a3993e37bbe397a1
Angel Kurten <angelkurten@hotmail.com>
2013-11-01 01:38:14 -0500

    dev modal

--- dev/app/views/principal.php
+++ dev/app/views/principal.php
@@ -1,43 +1,43 @@
 <!DOCTYPE HTML>
 <html>
-<head>
-  <meta charset="UTF-8" />
-  <!--[if lte IE 8]><script src="<?php echo base_url(); ?>js/index/html5shiv.js"></script><![endif]-->
-    <script src="<?php echo base_url(); ?>js/index/jquery.min.js"></script>
-    <script src="<?php echo base_url(); ?>js/index/jquery.dropotron.js"></script>
-    <script src="<?php echo base_url(); ?>js/index/skel.min.js"></script>
-    <script src="<?php echo base_url(); ?>js/index/skel-panels.min.js"></script>
-    <script src="<?php echo base_url(); ?>js/index/init.js"></script>
-    
-    <link rel="stylesheet" href="<?php echo base_url(); ?>css/modal/bootstrap-combined.min.css">
-    <link rel="stylesheet" href="<?php echo base_url(); ?>css/modal/bootstrap-switch.css">
+  <head>
+    <meta charset="UTF-8" />
+    <!--[if lte IE 8]><script src="<?php echo base_url(); ?>js/index/html5shiv.js"></script><![endif]-->
+      <script src="<?php echo base_url(); ?>js/index/jquery.min.js"></script>
+      <script src="<?php echo base_url(); ?>js/index/jquery.dropotron.js"></script>
+      <script src="<?php echo base_url(); ?>js/index/skel.min.js"></script>
+      <script src="<?php echo base_url(); ?>js/index/skel-panels.min.js"></script>
+      <script src="<?php echo base_url(); ?>js/index/init.js"></script>
+      
+      <link rel="stylesheet" href="<?php echo base_url(); ?>css/modal/bootstrap-combined.min.css">
+      <link rel="stylesheet" href="<?php echo base_url(); ?>css/modal/bootstrap-switch.css">
 
-    <link rel="stylesheet" href="<?php echo base_url(); ?>css/index/style.css" />
-    <noscript>
-      <link rel="stylesheet" href="<?php echo base_url(); ?>css/index/skel-noscript.css" />
       <link rel="stylesheet" href="<?php echo base_url(); ?>css/index/style.css" />
-      <link rel="stylesheet" href="<?php echo base_url(); ?>css/index/style-noscript.css" />
-    </noscript>
+      <noscript>
+        <link rel="stylesheet" href="<?php echo base_url(); ?>css/index/skel-noscript.css" />
+        <link rel="stylesheet" href="<?php echo base_url(); ?>css/index/style.css" />
+        <link rel="stylesheet" href="<?php echo base_url(); ?>css/index/style-noscript.css" />
+      </noscript>
 
-    <meta http-equiv="X-UA-Compatible" content="IE=9; IE=8;" />
-    <script src="<?php echo base_url(); ?>js/modal/bootstrap-switch.js"></script> 
-  
-    <!--[if lte IE 8]><link rel="stylesheet" href="<?php echo base_url(); ?>css/index/css/ie8.css" /><![endif]-->
-</head>
-<body class="homepage">
+      <meta http-equiv="X-UA-Compatible" content="IE=9; IE=8;" />
+      <script src="<?php echo base_url(); ?>js/modal/bootstrap-switch.js"></script> 
+    
+      <!--[if lte IE 8]><link rel="stylesheet" href="<?php echo base_url(); ?>css/index/css/ie8.css" /><![endif]-->
+  </head>
+  <body class="homepage">
     <div id="salida">
       <div id="header">
-      <div class="inner">
-        <header>
-          <h1 id="logo">Fundación Girucode</h1>
-          <h5>v2.0 Developed by<a href="https://www.facebook.com/angel.kurten?ref=tn_tnmn"> Angel Kurten</a></h5>
-          <hr />
-          <span class="byline">
-            Bienvenid@ <?php echo $user['name'] ?>, esta aplicacion te permite mantenerte en contacto con nosotros, 
-            permitiendonos informarte de cualquier evento realizado por Girucode.
-          </span>
-        </header>
-        <footer>
+        <div class="inner">
+          <header>
+            <h1 id="logo">Fundación Girucode</h1>
+            <h5>v2.0 Developed by<a href="https://www.facebook.com/angel.kurten?ref=tn_tnmn"> Angel Kurten</a></h5>
+            <hr />
+            <span class="byline">
+              Bienvenid@ <?php echo $user['name'] ?>, esta aplicacion te permite mantenerte en contacto con nosotros, 
+              permitiendonos informarte de cualquier evento realizado por Girucode.
+            </span>
+          </header>
+          <footer>
           <?php
             if(@$_GET['live']=='yes') {
           ?>
@@ -46,36 +46,45 @@
           <?php
             }
           ?>
-        </footer>
-      </div>
-      <nav id="nav">
-      <ul>
-        <li>
-          <span>Mas de Girucode</span>
+          </footer>
+        </div>
+        <nav id="nav">
           <ul>
-            <li><a href="http://girucode.me" target="_BLANK">Web Oficial</a></li>
-            <li><a href="http://girucode.tv" target="_BLANK">GiruTV</a></li>
             <li>
-              <span>Redes Sociales -></span>
+              <span>Mas de Girucode</span>
               <ul>
-                <li><a href="https://www.facebook.com/groups/GiruCode/" target="_BLANK">Grupo Facebook</a></li>
-                <li><a href="https://www.facebook.com/Girucode?ref=hl" target="_BLANK">Pagina Facebook</a></li>
-                <li><a href="http://twitter.com/Girucode" target="_BLANK">Twitter</a></li>
-                <li><a href="http://www.youtube.com/channel/UCx6FnLyN5lk4DaHMNQKZZWQ?feature=watch" target="_BLANK">Youtube</a></li>
-              </ul>
+                <li><a href="http://girucode.me" target="_BLANK">Web Oficial</a></li>
+                <li><a href="http://girucode.tv" target="_BLANK">GiruTV</a></li>
+                <li>
+                  <span>Redes Sociales -></span>
+                  <ul>
+                    <li><a href="https://www.facebook.com/groups/GiruCode/" target="_BLANK">Grupo Facebook</a></li>
+                    <li><a href="https://www.facebook.com/Girucode?ref=hl" target="_BLANK">Pagina Facebook</a></li>
+                    <li><a href="http://twitter.com/Girucode" target="_BLANK">Twitter</a></li>
+                    <li><a href="http://www.youtube.com/channel/UCx6FnLyN5lk4DaHMNQKZZWQ?feature=watch" target="_BLANK">Youtube</a></li>
+                  </ul>
+                </li>
+              </ul>          
             </li>
-          </ul>          
-        </li>
-        <li><a href="#myModal" role="button" data-toggle="modal">Panel de Control</a></li>
-      </ul>
-    </nav>    
-    </div>
+            <li><a href="#myModal" role="button" data-toggle="modal">Panel de Control</a></li>
+          </ul>
+        </nav>    
+      </div>
     </div>
 
-    <div class="modal-body" id="myModal">
-        <div class="make-switch">
-            <input type="checkbox" checked>
+    <div id="myModal" class="modal hide fade in" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="false" style="display: block;">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+        <h3>Modal</h3>
+      </div>
+      <div class="modal-body">
+        <div class="make-switch has-switch">
+          <div class="switch-animate switch-on"><input type="checkbox" checked=""><span class="switch-left">ON</span><label>&nbsp;</label><span class="switch-right">OFF</span></div>
         </div>
+      </div>
+      <div class="modal-footer">
+        <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+      </div>
     </div>
-</body>
+  </body>
 </html>
